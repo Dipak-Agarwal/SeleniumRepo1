16ce1e4f79784af65d2145e695d23ec5c48b944e
Mukesh_50 <Mukesh_50@192.168.0.101>
2017-04-23 11:10:25 +0530

    new TC Added

---
+++ com.learnautomation/src/test/java/testcases/Mukesh.java
@@ -0,0 +1,14 @@
+package testcases;
+
+public class Mukesh {
+
+	public Mukesh() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
